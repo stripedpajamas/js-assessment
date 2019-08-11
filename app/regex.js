cb28c76dba48b7620eaa8e263945a8360c1a02e1
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-08-11 09:56:04 -0700

    Solve regex

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return /\d/.test(str)
   },
 
   containsRepeatingLetter: function(str) {
-
+    return /([a-zA-Z])\1+/.test(str)
   },
 
   endsWithVowel: function(str) {
-
+    return /[aeiou]$/i.test(str)
   },
 
   captureThreeNumbers: function(str) {
-
+    return (/(\d{3})/.exec(str) || [])[1] || false
   },
 
   matchesPattern: function(str) {
-
+    return /^\d{3}-\d{3}-\d{4}$/.test(str)
   },
 
   isUSD: function(str) {
-
+    return /^\$(?:(?:(?:\d{1,3},)?(?:\d{3},)*\d{3})|\d{1,3})(?:\.\d{2})?$/.test(str)
   }
 };
